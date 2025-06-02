bc914266f3674eb4c728a90e817c09ead6cc1f45
Yerzhan <yerzhxn@gmail.com>
2025-06-02 12:26:22 +0500

    asd

--- src/views/youTube/YouTube.vue
+++ src/views/youTube/YouTube.vue
@@ -4,7 +4,7 @@
     <img
       src="../../assets/img/oyu_2.png"
       alt=""
-      class="absolute w-[600px] h-[600px] top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2"
+      class="absolute  md:w-[600px] h-[600px] top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 -z-10"
     />
     <div class="flex justify-between items-center">
       <p class="text-[24px]">You-Tube</p>
