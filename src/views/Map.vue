5a5a47c5a43de17817a85d5ee4f9be7181e33a13
Yerzhan <yerzhxn@gmail.com>
2025-06-02 11:15:05 +0500

    asd

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="absolute mt-[120px] ml-[120px] backdrop-blur-md p-[25px] rounded-xl space-y-[20px]">
-    <p class="text-[20px]">Бізбен байланыс</p>
-    <p class="flex items-center"><AimOutlined class="mr-[10px] text-[20px]"/> Мекен жай</p>
+  <div class="hidden md:grid absolute mt-[120px] ml-[20px] md:ml-[120px] backdrop-blur-md p-[25px] rounded-xl space-y-[20px]">
+    <p class="text-[16px] md:text-[20px]">Бізбен байланыс</p>
+    <p class="flex items-center"><AimOutlined class="mr-[10px] text-[16px] md:text-[20px]"/> Мекен жай</p>
     <p>Астана қаласы, Сауран көшесі, 7 А</p>
     <p class="flex items-center"><MailOutlined class="mr-[10px] text-[20px]"/>э-пошта:</p>
     <a href=""><p>info@ruhaniat.kz</p></a>
